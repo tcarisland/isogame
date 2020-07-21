5283774f2060aa43d8220da8eeeb2a9c1f794aaf
tcarisland <thor.arisland@aurorainnovation.com>
2020-07-21 07:08:09 +0200

    refactoring

--- src/model/ISOTile.tsx
+++ src/model/ISOTile.tsx
@@ -3,62 +3,7 @@ import Color from "../interfaces/Color";
 import grass0002 from "../resources/images/tiles/grass0002.png";
 import grass0003 from "../resources/images/tiles/grass0003.png";
 import facetile0000 from "../resources/images/tiles/facetile0000.png";
-
-export interface ISOTileVertex {
-    x: number,
-    y: number
-}
-
-export class ISOTileVertices {
-    nw: ISOTileVertex;
-    ne: ISOTileVertex;
-    se: ISOTileVertex;
-    sw: ISOTileVertex;
-    tileWidth: number;
-    tileHeight: number;
-
-    constructor(row: number, column: number, tileWidth: number, tileHeight: number) {
-        this.nw = {x: column * tileWidth, y: row * tileHeight};
-        this.ne = {x: (column * tileWidth) + tileWidth, y: row * tileHeight};
-        this.se = {x: (column * tileWidth) + tileWidth, y: (row * tileHeight) + tileHeight};
-        this.sw = {x: column * tileWidth, y: (row * tileHeight) + tileHeight};
-        this.tileWidth = tileWidth;
-        this.tileHeight = tileHeight;
-    }
-
-    public getVertices(): ISOTileVertex[] {
-        return [ this.nw, this.ne, this.se, this.sw ];
-    }
-
-    public getIsoVertices(): ISOTileVertex[] {
-        return [
-            { x: this.isoX(this.nw.x, this.nw.y), y: this.isoY(this.nw.x, this.nw.y) },
-            { x: this.isoX(this.ne.x, this.ne.y), y: this.isoY(this.ne.x, this.ne.y) },
-            { x: this.isoX(this.se.x, this.se.y), y: this.isoY(this.se.x, this.se.y) },
-            { x: this.isoX(this.sw.x, this.sw.y), y: this.isoY(this.sw.x, this.sw.y) }
-        ];
-    }
-
-    private isoX(xc: number, yc: number) {
-        return xc - yc;
-    };
-    private isoY(xc: number, yc: number) {
-        return (xc + yc) / 2;
-    }
-
-    public getEnclosingBox(): ISOTileVertex[] {
-        let minX = this.isoX(this.sw.x, this.sw.y);
-        let minY = this.isoY(this.nw.x, this.nw.y);
-        let maxX = this.isoX(this.ne.x, this.ne.y);
-        let maxY = this.isoY(this.se.x, this.se.y);
-        return [
-            {x: minX, y: minY},
-            {x: maxX, y: minY},
-            {x: maxX, y: maxY},
-            {x: minX, y: maxY}    
-        ]
-    }
-}
+import { ISOTileVertices } from "./ISOTileVertices";
 
 export default class ISOTile {
     row: number;

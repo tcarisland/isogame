519db043910fc5486646d0f2b941c9e5adbd38f4
tcarisland <thor.arisland@aurorainnovation.com>
2020-07-21 10:42:11 +0200

    started working on mouse listener

--- src/components/ArrowKeyButton.tsx
+++ src/components/ArrowKeyButton.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import ArrowKeyProps from '../interfaces/ArrowKeyProps';
 import ReactHtmlParser from 'react-html-parser';
 
-class ArrowsKeyButton extends React.Component<ArrowKeyProps> {    
-  render() {
-    return (<div id={this.props.keyId} className="arrowsKeyPreview" style={Object.assign({ gridArea: this.props.position })}>
+class ArrowsKeyButton extends React.Component<ArrowKeyProps> {
+  arrowKeyClicked() {
+    if(this !== undefined) {
+      console.log(this.props.keyId + "clicked");
+      console.log(this.props);
+    }
+  }
+render() {
+    return (<div
+      id={this.props.keyId}
+      onClick={this.arrowKeyClicked.bind(this)}
+      className="arrowsKeyPreview" 
+      style={Object.assign({ gridArea: this.props.position }, {cursor: "pointer"})}>
       {ReactHtmlParser(this.props.symbol)}
     </div>);
   }
